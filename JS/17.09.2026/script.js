831ac3abb3ef3a5a9167477b6bc53bd11efaad46
4EIStanislawSosnowski <lamberdzak114@gmail.com>
2026-09-17 09:23:33 +0200

    Add files via upload

---
+++ JS/17.09.2026/script.js
@@ -0,0 +1,9 @@
+//Ola
+let imie = prompt('Podaj imię')
+let klasa = prompt('Podaj klasę')
+
+document.write(imie + "<br>")
+document.write(klasa + "<br>")
+document.write('To moja wizytówka')
+
+console.log(imie + " " + klasa)
